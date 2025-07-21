8fd66b47bd9637d961aed04869f2ae15b5fbd3a2
mada-sys <madalina.vasile@student.unitbv.ro>
2025-07-21 18:21:55 +0300

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+locals {
+  prefix    = "mada"
+  filename  = "fisier.txt"
+  file_path = "${path.module}/${local.filename}"
+}
+variable "client_secret" {
+    type = string
+    sensitive = true
+  
+}
+variable "client_id" {
+    type = string
+    sensitive = true
+  
+}
+variable "tenant_id" {
+    type = string
+    sensitive = true
+  
+}
+variable "subscription_id" {
+    type = string
+    sensitive = true
+  
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "${local.prefix}-rg2"
+  location = "West Europe"
+}
