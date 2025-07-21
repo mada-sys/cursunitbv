63bdc76305a845dc0deb946e9e45d26c90b73c30
mada-sys <madalina.vasile@student.unitbv.ro>
2025-07-21 19:53:58 +0300

    ffinal

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,26 +3,7 @@ locals {
   filename  = "fisier.txt"
   file_path = "${path.module}/${local.filename}"
 }
-variable "client_secret" {
-    type = string
-    sensitive = true
-  
-}
-variable "client_id" {
-    type = string
-    sensitive = true
-  
-}
-variable "tenant_id" {
-    type = string
-    sensitive = true
-  
-}
-variable "subscription_id" {
-    type = string
-    sensitive = true
-  
-}
+
 terraform {
   required_providers {
     azurerm = {
@@ -34,15 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg2" {
-  count = 2
+  count    = 2
   name     = "${local.prefix}-resources-${count.index}"
   location = "West Europe"
 }
